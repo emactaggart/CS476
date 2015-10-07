3f5b30020733b6526813257a9101a03dd578335c
emactaggart <evan.mactaggart@gmail.com>
2015-10-07 14:04:01 -0600

    Added example WCF project with client, host, and library;

---
+++ example/GettingStartedHost/GettingStartedHost/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using GettingStartedLib;
+using System.ServiceModel.Description;
+
+namespace GettingStartedHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
