a0a44ddd690b0dcc650fcd5d8d0a4522f2a2ebc6
emactaggart <evan.mactaggart@gmail.com>
2015-10-21 10:58:58 -0600

    Added ADO.NET Entity Framework quickstart tutorial project.

---
+++ example/CourseManager/CourseManager/CourseViewer.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CourseManager
+{
+    partial class CourseViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeForm = new System.Windows.Forms.Button();
+            this.departmentList = new System.Windows.Forms.ComboBox();
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            this.saveChanges = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // closeForm
+            // 
+            this.closeForm.Location = new System.Drawing.Point(133, 176);
+            this.closeForm.Name = "closeForm";
+            this.closeForm.Size = new System.Drawing.Size(75, 23);
+            this.closeForm.TabIndex = 0;
+            this.closeForm.Text = "Close";
+            this.closeForm.UseVisualStyleBackColor = true;
+            this.closeForm.Click += new System.EventHandler(this.closeForm_Click);
+            // 
+            // departmentList
+            // 
+            this.departmentList.FormattingEnabled = true;
+            this.departmentList.Location = new System.Drawing.Point(95, 98);
+            this.departmentList.Name = "departmentList";
+            this.departmentList.Size = new System.Drawing.Size(121, 24);
+            this.departmentList.TabIndex = 1;
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courseGridView.Location = new System.Drawing.Point(133, 262);
+            this.courseGridView.Name = "courseGridView";
+            this.courseGridView.RowTemplate.Height = 24;
+            this.courseGridView.Size = new System.Drawing.Size(240, 150);
+            this.courseGridView.TabIndex = 2;
+            this.courseGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.courseGridView_CellContentClick);
+            // 
+            // saveChanges
+            // 
+            this.saveChanges.Location = new System.Drawing.Point(514, 176);
+            this.saveChanges.Name = "saveChanges";
+            this.saveChanges.Size = new System.Drawing.Size(75, 23);
+            this.saveChanges.TabIndex = 3;
+            this.saveChanges.Text = "Update";
+            this.saveChanges.UseVisualStyleBackColor = true;
+            this.saveChanges.Click += new System.EventHandler(this.saveChanges_Click);
+            // 
+            // CourseViewer
+            // 
+            this.ClientSize = new System.Drawing.Size(763, 482);
+            this.Controls.Add(this.saveChanges);
+            this.Controls.Add(this.courseGridView);
+            this.Controls.Add(this.departmentList);
+            this.Controls.Add(this.closeForm);
+            this.Name = "CourseViewer";
+            this.Text = "Course Viewer";
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeForm;
+        private System.Windows.Forms.ComboBox departmentList;
+        private System.Windows.Forms.DataGridView courseGridView;
+        private System.Windows.Forms.Button saveChanges;
+    }
+}
+
